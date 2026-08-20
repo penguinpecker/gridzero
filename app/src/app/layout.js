3fdc8f94fef97e950998c6d22e2f430e55819792
penguinpecker <penguinpecker1@gmail.com>
2026-08-20 11:10:42 +0530

    add base:app_id meta tag for Base App ownership verification

--- app/src/app/layout.js
+++ app/src/app/layout.js
@@ -10,6 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="base:app_id" content="6a8690066ea1f57fed333a12" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
